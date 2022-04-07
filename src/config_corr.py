52833ccb8338ab6a0f73548748632fb7717170d4
Klaudia <klaudia.dilcher@gmail.com>
2022-04-07 15:42:01 +0200

    Correlated KF

--- src/config_corr.py
+++ src/config_corr.py
@@ -12,16 +12,36 @@ config.simulation = {
     'x_0': np.array([5., 0., 8.]),  # initial state vector [Jx, Jy, omega]
     't_0': 0,
     'noise': {'Q_m': 0.01,
-              'Q_freq': 0.01},
+              'Q_freq': 0.0,
+              'mean': np.array([0.0, 0.0, 0.0])},
     'measurement': {'measurement_strength': 1., # eta
-                    'H': np.array([[0., 1., 0.]])
-                    },
-    'filter': {}
+                    'H': np.array([[0., 1., 0.]]),
+                    'noise': {'mean': 0.0}
+                    }
 }
 
 config.filter_ekf = {
+    'dt': config.simulation['dt'],
+    'decoherence_x': config.simulation['decoherence_x'],
+    'decoherence_y': config.simulation['decoherence_y'],
+    'frequency_decay_rate': config.simulation['frequency_decay_rate'],
+    't_0': config.simulation['t_0'],
     'x_0': np.array([5.0, 0.0, 8.1]),
     'P0': np.array([[0.0, 0., 0.],
                     [0., 0.0, 0.],
                     [0., 0., 0.01]]),
-    'noise': {'B': np.identity(3)}}
+    'noise': {'Q': np.array([[config.simulation['noise']['Q_m'], config.simulation['noise']['Q_m'], 0.],
+                             [config.simulation['noise']['Q_m'], config.simulation['noise']['Q_m'], 0.],
+                             [0., 0., config.simulation['noise']['Q_freq']]]),
+              'mean': np.array([0.0, 0.0, 0.0]),
+              'B': np.identity(3),
+              'S': np.multiply(np.sqrt(config.simulation['measurement']['measurement_strength']), np.array([[config.simulation['noise']['Q_m'],
+                                                                                                            config.simulation['noise']['Q_m'],
+                                                                                                            0.]]))
+              },
+    'measurement': {'measurement_strength': config.simulation['measurement']['measurement_strength'],
+                    'H': config.simulation['measurement']['H'],
+                    'dim_z': 1,
+                    'R': np.array([[config.simulation['measurement']['measurement_strength']*config.simulation['noise']['Q_m']]])
+                    }
+}
--- src/kalman_filter/continuous/corr_simple_model_ekf.py
+++ src/kalman_filter/continuous/corr_simple_model_ekf.py
@@ -11,7 +11,7 @@ class CorrSimpleModelEKF(EKF):
         self.model_params = model_params
         self._F = self.F(self._x, model_params)
         self._B = model_params.noise.B
-        self._S = np.array([[], [], []])
+        self._S = model_params.noise.S
 
     @staticmethod
     def F(x, model_params):
@@ -32,31 +32,31 @@ class CorrSimpleModelEKF(EKF):
         return CorrSimpleModelEKF.fx(x, model_params) + np.dot(K, y) / dt
 
     @staticmethod
-    def dP_dt(P, t, x, B, S, R_inv, H, Q, dim_x, model_params):
-        P = np.reshape(P, (dim_x, dim_x))
-        temp = CorrSimpleModelEKF.F(x, model_params) - np.dot(B, S).dot(R_inv).dot(H)
-        P = np.dot(temp, P) + np.dot(P, temp.T) - np.dot(P, H.T).dot(R_inv).dot(H).dot(P) + B.dot(Q - S.dot(R_inv).dot(S.T)).dot(B.T)
+    def dP_dt(P, t, x, K, B, H, Q, dim_x, model_params):
+        P = np.reshape(np.dot(CorrSimpleModelEKF.F(x, model_params),
+                              np.reshape(P, (dim_x, dim_x)))
+                       + np.dot(np.reshape(P, (dim_x, dim_x)),
+                                np.transpose(CorrSimpleModelEKF.F(x, model_params)))
+                       - np.dot(np.dot(K, H), np.reshape(P, (dim_x, dim_x))) + np.dot(B, Q).dot(B.T), dim_x ** 2)
         return np.reshape(P, dim_x ** 2)
 
     def predict_update(self, dz):
         self._dz = copy.deepcopy(dz)
-        self._K = np.dot(np.dot(self._P, self._H.T) + np.dot(self._B, self._S), self._R_inv)
+        self._K = np.dot(np.dot(self._P, self._H.T) + np.dot(self._B, self._S.T), self._R_inv)
         self._y = dz - self._measurement_strength * np.dot(self._H, self._x) * self._dt
         t = np.linspace(self._t, self._t + self._dt, num=20)
         P = odeint(CorrSimpleModelEKF.dP_dt,
                    np.reshape(self._P, self._dim_x ** 2),
                    t,
                    args=(self._x,
+                         self._K,
                          self._B,
-                         self._S,
-                         self._R_inv,
                          self._H,
                          self._Q,
                          self._dim_x,
                          self.model_params))[1, :]
         x = odeint(CorrSimpleModelEKF.dx_dt, self._x, t, args=(self._K, self._y, self._dt, self.model_params))[-1, :]
         self._x = x
-        # dP = self.dx_dt()
         self._P = np.reshape(P, (self._dim_x, self._dim_x))
         self._t += self._dt
         return
