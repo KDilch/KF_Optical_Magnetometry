c049dcfffa50279009fba970f206d501f5aa6533
Klaudia <klaudia.dilcher@gmail.com>
2026-08-10 11:28:30 +0200

    Clean up repository, keeping only the unitless magnetometer simulation and EKF filter

--- src/MATLAB_inference/data_transform.py
+++
@@ -1,9 +0,0 @@
-
-import numpy as np
-def data_transform(T2, N, q, g_D, Sph, T0):
-    Nq = N * q
-    xcoef = np.sqrt(2 / Nq)
-    ycoef = np.sqrt(2 / (g_D**2 * Nq))
-    sig_v = np.sqrt(2 * Sph / (g_D**2 * Nq * T0))
-    T01 = T0 / T2
-    return xcoef, ycoef, sig_v, T01
